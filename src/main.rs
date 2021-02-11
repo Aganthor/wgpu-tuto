2f2e30e57faab10d9648b9571cc564a1b61570b1
Luc Bergeron <bergeronl@college-montreal.qc.ca>
2021-02-11 14:50:15 -0500

    Done til the update method.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::mem::swap;
+use futures::executor::block_on;
 use winit::{
     event::*,
     event_loop::{EventLoop, ControlFlow},
@@ -16,7 +18,7 @@ struct State {
 impl State {
     async fn new(window: &Window) -> Self {
         let size = window.inner_size();
-
+        
         let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
         let surface = unsafe { instance.create_surface(window) };
         let adapter = instance.request_adapter(
@@ -25,64 +27,104 @@ impl State {
                 compatible_surface: Some(&surface),
             },
         ).await.unwrap();
-
+        
         let (device, queue) = adapter.request_device(
             &wgpu::DeviceDescriptor {
-                features: &wgpu::Features::empty(),
-                limits: wpgu::Limits::default(),
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::default(),
                 shader_validation: true,
             },
             None,
         ).await.unwrap();
-
+        
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+        
+        Self {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size,
+        }
+        
     }
-
+    
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        unimplemented!()
+        self.size = new_size;
+        self.sc_desc.width= new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
     }
-
+    
     fn input(&mut self, event: &WindowEvent) -> bool {
-        unimplemented!()
+        false
     }
-
+    
     fn update(&mut self) {
-        unimplemented!()
+        todo!()
     }
-
-    fn render(&mut self) {
-        unimplemented!()
+    
+    fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
+        let frame = self
+            .swap_chain
+            .get_current_frame()?
+            .output;
+        Ok(())
     }
 }
 
 fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
-        .build(&event_loop)
-        .unwrap();
-
+    .build(&event_loop)
+    .unwrap();
+    
+    let mut state = block_on(State::new(&window));
+    
     event_loop.run(move |event, _, control_flow| {
         match event {
             Event::WindowEvent {
                 ref event,
                 window_id,
-            } if window_id == window.id() => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::KeyboardInput {
-                    input,
-                    ..
-                } => {
-                    match input {
-                        KeyboardInput {
-                            state: ElementState::Pressed,
-                            virtual_keycode: Some(VirtualKeyCode::Escape),
-                            ..
-                        } => *control_flow = ControlFlow::Exit,
-                        _ => {}
+            } if window_id == window.id() => if !state.input(event) {
+                match event {
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
                     }
+                    
+                    WindowEvent::ScaleFactorChanged {new_inner_size, .. } => {
+                        // new_inner-size is &&mut so we have to dereference it twice
+                        state.resize(**new_inner_size);
+                    }
+                    
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    
+                    WindowEvent::KeyboardInput {
+                        input,
+                        ..
+                    } => {
+                        match input {
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                            _ => {}
+                        }
+                    }
+                    _ => {}
                 }
+            }
                 _ => {}
             }
-            _ => {}
-        }
-    });
-}
+        });
+    }
+    
